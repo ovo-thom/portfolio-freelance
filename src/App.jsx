22780f0f52086a6dbb59f3061944e2dc9cc90630
Thomas OvO <thonnad.thomas@gmail.com>
2025-03-31 19:01:02 +0200

    feat: add Training.jsx style and responsive and add React-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Skills from './components/Skills';
@@ -5,20 +6,24 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import About from './components/About';
 import Footer from './components/Footer';
+import Training from './components/Training';
 
 function App() {
   
 
   return (
+    <Router>
     <div>
       <Navbar />
       <Hero />
       <About />
       <Skills />
       <Projects />
+      <Training />
       <Contact />
       <Footer />
     </div>
+    </Router>
   )
 }
 
