2b5e32b68e978e1cd80a945fb117698c8e08f513
Thomas OvO <thonnad.thomas@gmail.com>
2025-04-25 19:02:17 +0200

    feat: add React Udemy certificate into TrainingDetails.jsx

--- src/components/TrainingDetails.jsx
+++ src/components/TrainingDetails.jsx
@@ -1,5 +1,6 @@
 import becode from "../assets/becode.png";
 import sql from "../assets/sql.png";
+import react from "../assets/Certificate_React_Udemy.jpg";
 
 function TrainingDetails() {
   const trainings = [
@@ -17,6 +18,13 @@ function TrainingDetails() {
         "Formation à distance de 4 semaines sur la manipulation des bases de données.",
       image: sql,
     },
+    {
+      title: "Udemy",
+      date: "23 avril 2025",
+      description:
+        "React de A à Z ainsi que Redux avec un projet à la fin, un générateur d'ombres interactif.",
+      image: react,
+    },
   ];
 
   return (
@@ -27,19 +35,22 @@ function TrainingDetails() {
             key={index}
             className="w-full sm:w-1/2 sm:h-54 justify-center items-center flex flex-col mb-12"
           >
-            <h3 className="text-gray-50 text-lg sm:text-xl md:text-2xl font-semibold">{training.title}</h3>
+            <h3 className="text-gray-50 text-lg sm:text-xl md:text-2xl font-semibold">
+              {training.title}
+            </h3>
             <div className="w-1/2 rounded-lg py-4">
               <img
                 src={training.image}
                 alt={`diplôme ${training.title}`}
-                className="w-full max-w-[300px] sm:h-[240px] md:h-[350px] mx-auto object-contain rounded-lg transition-transform duration-300 ease-in-out hover:scale-125"
+                // className="w-full max-w-[300px] sm:h-[240px] md:h-[350px] mx-auto object-contain rounded-lg transition-transform duration-300 ease-in-out hover:scale-125"
+                className="w-[300px] sm:h-[240px] md:h-[300px] object-contain rounded-lg transition-transform duration-300 ease-in-out hover:scale-125"
               />
             </div>
-            <div className="w-2/3 sm:h-48 lg:h-44 shadow-lg shadow-[#235060] p-3 sm:p-5 rounded-lg">
-              <p className="text-[#235060] text-sm text-center">
+            <div className="w-2/3 md:h-auto shadow-lg shadow-[#235060] p-3 sm:p-5 rounded-lg">
+              <p className="text-[#164252] text-sm text-center">
                 {training.date}
               </p>
-              <p className="my-5 w-full text-gray-100 text-center text-[13px] sm:text-sm lg:text-lg">
+              <p className="my-5 w-full text-gray-100 text-center text-[13px] sm:text-sm lg:text-base">
                 {training.description}
               </p>
             </div>
