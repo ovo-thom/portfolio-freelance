82641c9922f2b77f9a7b8dd2b0f1daf54c030f2f
Thomas OvO <thonnard.thomas@gmail.com>
2025-06-25 23:59:40 +0200

    style: add technos and short description and responsive into ProjectsPage.jsx

--- src/components/ProjectsPage.jsx
+++ src/components/ProjectsPage.jsx
@@ -4,13 +4,23 @@ import projects from "../data/projectData";
 export default function ProjectsPage() {
   return (
     <div className="min-h-screen bg-[#080808] text-white p-8 w-full">
-      <div className="flex justify-around space-x-5 w-[95%] mx-auto mt-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-[95%] mx-auto mt-20">
         {projects.map((project) => (
           <Link key={project.id} to={`/projects/${project.id}`}>
             <div className="relative border border-gray-700 rounded-xl p-3 group h-full w-full">
               <h3 className="text-sm sm:text-base md:text-lg xl:text-xl text-center text-gray-50 font-semibold ">
                 {project.title}
               </h3>
+              <div className="flex flex-wrap justify-center gap-1 mb-4 mt-2">
+                {project.technologies.slice(0, 3).map((tech) => (
+                  <span
+                    key={tech}
+                    className="text-xs sm:text-sm text-gray-300 bg-gray-800/50 px-2 py-1 rounded"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
               <div className="relative m-4 overflow-hidden rounded">
                 <img
                   src={project.image}
@@ -21,6 +31,9 @@ export default function ProjectsPage() {
                   Voir le projet
                 </div>
               </div>
+              <p className="text-xs sm:text-sm md:text-base text-gray-400 text-center mb-3 line-clamp-1">
+                {project.description.substring(0, 60)}...
+              </p>
             </div>
           </Link>
         ))}
