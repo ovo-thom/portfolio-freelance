5a09c1cd5a080f4848a527362d77655e4abd25cc
Thomas OvO <thonnard.thomas@gmail.com>
2025-08-02 18:31:24 +0200

    chore: remove Skills.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import About from "./components/About";
@@ -20,7 +19,6 @@ function App() {
             <>
               <Hero />
               <About />
-              <Skills />
               <Projects />
               <Contact />
             </>
